0c9eadef9b692e2bf651deebd9e7e14e6b619a94
Stevelmans <bizz.paul@gmail.com>
2016-10-24 10:52:40 +0200

    Various

--- app/src/main/java/com/projects/stevelmans/monitor/LoginActivity.java
+++ app/src/main/java/com/projects/stevelmans/monitor/LoginActivity.java
@@ -1,48 +1,15 @@
 package com.projects.stevelmans.monitor;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Handler;
-import android.os.Parcel;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -51,7 +18,6 @@ public class LoginActivity extends AppCompatActivity {
 
     // UI references.
     private EditText mPasswordView;
-    private View mLoginFormView;
     SeekBar m_QuotaBar;
     EditText m_QuotaText;
 
@@ -70,16 +36,13 @@ public class LoginActivity extends AppCompatActivity {
         });
         m_QuotaBar = (SeekBar) findViewById(R.id.seekBarExtraTime);
         m_QuotaText = (EditText) findViewById(R.id.extraMinutesText);
-
-        mLoginFormView = findViewById(R.id.login_form);
+        m_QuotaBar.setOnSeekBarChangeListener(new QuotaBarListener());
 
         Tracker.LoginRunning = true;
         Tracker.Locked = true;
     }
 
     private class QuotaBarListener implements SeekBar.OnSeekBarChangeListener {
-        int progressChanged = 0;
-
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             m_QuotaText.setText(progress);
         }
